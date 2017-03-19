e6c5a74f667421288b00994897b60d9000806c6d
Cleatuer <nicole.wresnik@gmail.com>
2017-03-19 23:55:55 +0100

    initialize maven project

---
+++ src/main/java/shootme/gui/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shootme.gui;
+
+/**
+ *
+ * @author Nicole
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("bla");
+    }
+}
