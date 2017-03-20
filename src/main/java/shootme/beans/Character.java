eca6fdf9b7d8038c87880798451870469c1e1140
Cleatuer <nicole.wresnik@gmail.com>
2017-03-20 13:48:33 +0100

    created Character class and added character sprite

---
+++ src/main/java/shootme/beans/Character.java
@@ -0,0 +1,83 @@
+package shootme.beans;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by Nicole on 20.03.2017
+ */
+public class Character {
+    private int dx;
+    private int dy;
+    private int x;
+    private int y;
+    private Image image;
+
+    public Character() {
+        ImageIcon ii = new ImageIcon("/res/freeman.jpg");
+        image = ii.getImage();
+        x = 40;
+        y = 60;
+    }
+
+
+    public void move() {
+        x += dx;
+        y += dy;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = -1;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 1;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = -1;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 1;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = 0;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 0;
+        }
+    }
+}
