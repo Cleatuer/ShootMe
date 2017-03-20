ac588cb0088303fc55ba9fa2d71eb1e633df43a8
Cleatuer <nicole.wresnik@gmail.com>
2017-03-20 13:58:46 +0100

    created GamePanel class - override doesn't want to :(

---
+++ src/main/java/shootme/gui/GamePanel.java
@@ -0,0 +1,71 @@
+package shootme.gui;
+
+import shootme.beans.Character;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by Nicole on 20.03.2017
+ */
+public class GamePanel extends JPanel implements ActionListener {
+    private Timer timer;
+    private Character character;
+    private final int DELAY = 10;
+
+    public GamePanel() {
+
+        initBoard();
+    }
+
+    private void initBoard() {
+
+        addKeyListener(new TAdapter());
+        setFocusable(true);
+        setBackground(Color.BLACK);
+
+        character = new Character();
+
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        doDrawing(g);
+
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+    private void doDrawing(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawImage(character.getImage(), character.getX(), character.getY(), this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        character.move();
+        repaint();
+    }
+
+    private class TAdapter extends KeyAdapter {
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            character.keyReleased(e);
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            character.keyPressed(e);
+        }
+    }
+}
