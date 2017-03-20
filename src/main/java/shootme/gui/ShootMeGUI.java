a57c70ea740e1222107bd90d87b7350c2558da35
Cleatuer <nicole.wresnik@gmail.com>
2017-03-20 14:09:30 +0100

    created Main GUI

---
+++ src/main/java/shootme/gui/ShootMeGUI.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shootme.gui;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Nicole
+ */
+public class ShootMeGUI extends JFrame{
+    
+    public ShootMeGUI() {
+        initGUI();
+    }
+    
+    private void initGUI() {
+        
+        add(new GamePanel());
+        
+        setSize(400, 300);
+        setResizable(false);
+        
+        setTitle("Moving character!");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                
+                ShootMeGUI ex = new ShootMeGUI();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
