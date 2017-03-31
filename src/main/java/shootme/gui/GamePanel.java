f82ef67d0224645962640c2b42773df16a7b3f8a
Alexander Dietrich <diealb13@htl-kaindorf.ac.at>
2017-03-31 09:22:39 +0200

    tried to fix movment.... tried again

--- src/main/java/shootme/gui/GamePanel.java
+++ src/main/java/shootme/gui/GamePanel.java
@@ -51,7 +51,7 @@ public class GamePanel extends JPanel implements ActionListener
    {
       Graphics2D g2d = (Graphics2D) g;
       g2d.setColor(Color.white);
-      //g2d.drawRect(character.getX(), character.getY(), character.getImage().getWidth(), character.getImage().getHeight());
+      g2d.drawRect(character.getX(), character.getY(), character.getImage().getWidth(), character.getImage().getHeight());
       character.move();
       g2d.drawImage(character.getImage(), character.getX(), character.getY(), null);
    }
